0d6127e72e4219b29631518031e9940d63a3a966
Louis Sicard <lsicard@core-techs.fr>
2018-10-24 16:26:31 +0200

    Rename bundle

--- ADSSecurityBundle.php
+++ ADSSecurityBundle.php
@@ -4,7 +4,7 @@ namespace AdimeoDataSuite\Bundle\ADSSecurityBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class SecurityBundle extends Bundle
+class ADSSecurityBundle extends Bundle
 {
 
 }
